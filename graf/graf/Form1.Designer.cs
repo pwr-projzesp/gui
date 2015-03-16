d7c47d3e3d467274f34ad5a78737951a591bf382
kamyk205 <kam.matusiak@gmail.com>
2015-03-16 16:07:42 +0100

    16.03
    postepy po kolejnych zajeciach

---
+++ graf/graf/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace graf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(601, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj urządzenie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(601, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(678, 12);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 556);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ graf/graf/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graf
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        Point[] pts = new Point[1000];
+
+        Image baseStation = Image.FromFile("base.png");
+        Image nodeStation = Image.FromFile("node.png");
+
+        Font font = new Font("Arial", 8);
+        SolidBrush brush = new SolidBrush(Color.Black);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawBase(Point pts)
+        {
+            this.CreateGraphics().DrawImage(baseStation, pts.X, pts.Y);
+            this.CreateGraphics().DrawString("Base", font, brush, pts.X+18, pts.Y+baseStation.Height-20);
+        }
+
+        private void drawLine(Point startPts, Point endPts)
+        {
+            startPts.X = startPts.X + baseStation.Width / 2;
+            startPts.Y = startPts.Y + baseStation.Height / 2;
+
+            endPts.X = endPts.X + nodeStation.Width / 2;
+            endPts.Y = endPts.Y + nodeStation.Height / 2;
+
+            this.CreateGraphics().DrawLine(new Pen(Brushes.Black, 3), startPts, endPts);
+        }
+
+        private void drawNode(Point pts)
+        {
+            this.CreateGraphics().DrawImage(nodeStation, pts.X, pts.Y);
+            this.CreateGraphics().DrawString("Node", font, brush, pts.X+13, pts.Y+nodeStation.Height-20);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pts[count++] = new Point((int)numericUpDown1.Value, (int)numericUpDown2.Value);
+
+            for (int i=count-1; i >=0; i--)
+            {
+                if (i != 0)
+                {
+                    drawLine(pts[0], pts[i]);
+                    drawNode(pts[i]);
+                }
+                else
+                    drawBase(pts[0]);
+            }
+        }
+    }
+}
